05d24ee7208cc3ccf8767ec98fa00f1027301a34
billiebanditz <32966890+billiebanditz@users.noreply.github.com>
2017-10-24 17:09:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*
-copyright @ medantechno.com
-Modified by Ilyasa
+copyright @ bncautowork.com
+Modified by BillieDhikaS
 2017
 */
 require_once('./line_class.php');
@@ -49,7 +49,7 @@ if($url['result'] == 404)
 							'messages' => array(
 								array(
 										'type' => 'text',					
-										'text' => 'bahasa paan tuh.'
+										'text' => 'bahasa mana tuh.'
 									)
 							)
 						);
